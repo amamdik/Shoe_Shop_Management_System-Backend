b58d17dc7563789ed42108299f17a71d96144b45
amamdik <amamdik@gmail.com>
2024-05-21 14:35:04 +0530

    initial commit

---
+++ src/main/java/lk/ijse/gdse66/Shoe_Shop_Management_SystemBackend/ShoeShopManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Shoe_Shop_Management_SystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeShopManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse66/Shoe_Shop_Management_SystemBackend/ShoeShopManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Shoe_Shop_Management_SystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeShopManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
